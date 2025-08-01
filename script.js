cc713166e6c20a8d50147b1c53b963ab6f355c23
Elsabeth <143882270+eElsabethninaa@users.noreply.github.com>
2025-08-01 17:50:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-console.log()
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("JavaScript is connected and DOM is ready!");
+
+  const form = document.getElementById("registration-form");
+  const feedbackDiv = document.getElementById(id = "form-feedback");
+  const usernameInput = document.getElementById("username");
+  const emailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
+  
+
+  form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+
+  const username = usernameInput.value.trim();
+  const password = passwordInput.value.trim();
+  const email = emailInput.value.trim();
+
+  let isValid = true;
+  const messages = [];
+  
+  if (username.length < 3) {
+    isValid = false;
+    messages.push("Username must be at least 3 characters.");
+  }
+
+  if (!email.includes('@') || !email.includes('.')) {
+    isValid = false;
+    messages.push("Your email input is invalid!");
+  }
+  if (password.length < 8) {
+    isValid = false;
+    messages.push("Password must be at least 8 characters.");
+  }
+
+  feedbackDiv.style.display = "block";
+
+  if (isValid) {
+    feedbackDiv.textContent = "Registeration successful!";
+    feedbackDiv.style.color = "#28a745";
+} else {
+    loginFeedback.innerHTML = messages.join("<br>");
+    loginFeedback.style.color = "#dc3545";
+};
+ 
+
+});
+});
+
